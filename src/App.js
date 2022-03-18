9c19ffef1e3ef083f144d6b126231e037c56ffa6
abhi62 <abhi.leon.93@gmail.com>
2022-03-19 00:43:13 +0530

    code spliting

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import React, { Suspense, lazy } from 'react';
 import { makeStyles } from '@material-ui/core';
-import Homepage from './Pages/HomePage';
 import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
-import CoinPage from './Pages/CoinPage';
 import Header from './components/Header';
+// import Alert from './components/Alert';
+
+const Homepage = lazy(() => import('./Pages/HomePage'));
+const CoinPage = lazy(() => import('./Pages/CoinPage'));
 
 const useStyles = makeStyles(() => ({
   App: {
-    backgroundColor: 'black',
+    backgroundColor: '#14161a',
     color: 'white',
     minHeight: '100vh',
   },
@@ -20,9 +23,12 @@ function App() {
     <BrowserRouter>
       <div className={classes.App}>
         <Header />
-        <Route path='/' component={Homepage} exact />
-        <Route path='/coins/:id' component={CoinPage} exact />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Route path='/' component={Homepage} exact />
+          <Route path='/coins/:id' component={CoinPage} exact />
+        </Suspense>
       </div>
+      {/* <Alert /> */}
     </BrowserRouter>
   );
 }
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,13 +1,16 @@
-import React from "react";
-import Banner from "../components/Banner/Banner";
-import CoinsTable from "../components/CoinsTable";
+import React, { Suspense } from 'react';
+import Banner from '../components/Banner/Banner';
+
+const CoinsTable = React.lazy(() => import('../components/CoinsTable'));
 
 const Homepage = () => {
   return (
-    <>
+    <div>
       <Banner />
-      <CoinsTable />
-    </>
+      <Suspense fallback={<div>Loading...</div>}>
+        <CoinsTable />
+      </Suspense>
+    </div>
   );
 };
 
